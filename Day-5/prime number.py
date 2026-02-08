7edad942519c0fdb6ed3393599f4f272f6280da4
ananyarp1019 <ananya.rp1019@gmail.com>
2026-02-08 21:22:41 +0530

    Implement prime number check functionality
    Add functions to check if a number is prime and take user input.

--- Day-5/prime number.py
+++ Day-5/prime number.py
@@ -1,1 +1,26 @@
+def is_prime(number):
+    """Return True if number is prime, otherwise False."""
+    
+    if number <= 1:
+        return False  
+    
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False  
+    
+    return True  
+
+
+def check_prime():
+    """Take input from user and check if it is prime."""
+    
+    num = int(input("Enter a number: "))
+    
+    if is_prime(num):
+        print(f"{num} is a Prime Number.")
+    else:
+        print(f"{num} is NOT a Prime Number.")
+
+
+check_prime()
 
