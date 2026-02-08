544075d6b2773e2fc960e8957acfd20f4f95a976
ananyarp1019 <ananya.rp1019@gmail.com>
2026-02-08 20:45:02 +0530

    Update personal_details.py

--- Day-1/personal_details.py
+++ Day-1/personal_details.py
@@ -1,1 +1,6 @@
+print("Name: Ananya Praksha R")
+print("College: Visvesvaraya technological University VIAT muddenahalli chickballapur")
+print("Course: MCA")
+print("Email:ananya.rp1019@gmail.com ")
+print("Phone: 9900611374")
 
